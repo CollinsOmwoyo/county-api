ed115f5002615a1f904e3a36583ed886ff95a6ad
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-06-19 18:11:29 +0300

    feat: create User entity with properties and decorators for TypeORM

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { userRole } from "../enums/user-role.enum";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({ type: 'enum', enum: userRole, default: userRole.CITIZEN })
+    role: userRole;
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
