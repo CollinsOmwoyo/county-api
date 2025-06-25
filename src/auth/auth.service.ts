636633bb328a456a4ab49600b1e30d4640fc1b7c
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-06-25 10:52:12 +0300

    feat: implement registration endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '@users/dto/create-user.dto';
+import { UsersService } from '@users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UsersService) {}
+
+    async register(createUserDto: CreateUserDto) {
+        return this.userService.create(createUserDto);
+    }
+
+    // To later implement login functionality
+    /*
+    async login(createUserDto: CreateUserDto) {
+        return this.userService.findOne(createUserDto.email);
+    }
+    */
+}
