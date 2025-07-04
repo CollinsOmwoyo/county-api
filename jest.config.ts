1ebb7387e2f4a0aaecf8537a420a88092a052e19
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-07-04 11:14:16 +0300

    fix: correct formatting and indentation in jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -2,15 +2,15 @@ import { pathsToModuleNameMapper } from 'ts-jest';
 import { compilerOptions } from './tsconfig.json';
 
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+preset: 'ts-jest',
+testEnvironment: 'node',
+moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/src/',
-  }),
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  rootDir: './',
-  testMatch: ['**/*.spec.ts'],
-  transform: {
+}),
+moduleFileExtensions: ['ts', 'js', 'json'],
+rootDir: './',
+testMatch: ['**/*.spec.ts'],
+transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+},
 };
