c144ed82a1863288d081820c0c84399437d7aa14
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-07-04 16:41:28 +0300

    feat: set up Swagger documentation for County Services API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  const config = new DocumentBuilder()
+    .setTitle('County Services API')
+    .setDescription('API for citizen services, authentication and administration')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
