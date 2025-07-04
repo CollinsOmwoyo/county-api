99fe64bcf0b3a85d7e5c06855a8e10ff55bd1711
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-07-04 11:12:56 +0300

    feat: add passport-jwt dependency and jest configuration file

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src/',
+  }),
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  rootDir: './',
+  testMatch: ['**/*.spec.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
