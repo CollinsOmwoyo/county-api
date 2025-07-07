9d3ef25d5aee06a49e83413c6eacc1b5e43d4cbc
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-07-07 12:56:58 +0300

    test: enhance ServiceCatalogService tests with mock repository implementation

--- src/service-catalog/service-catalog.service.spec.ts
+++ src/service-catalog/service-catalog.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ServiceCatalog } from './entities/service-catalog.entity';
 import { ServiceCatalogService } from './service-catalog.service';
 
 describe('ServiceCatalogService', () => {
   let service: ServiceCatalogService;
 
+  const mockRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+    findOneBy: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ServiceCatalogService],
+      providers: [
+        ServiceCatalogService,
+        {
+          provide: getRepositoryToken(ServiceCatalog),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ServiceCatalogService>(ServiceCatalogService);
