7c3b7d8def4d5c7859c9185b83b76d8d8f8f9c13
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-07-07 13:04:06 +0300

    test: Attempt at avoiding entire dependency trigger

--- src/service-catalog/service-catalog.controller.spec.ts
+++ src/service-catalog/service-catalog.controller.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ServiceCatalogController } from './service-catalog.controller';
+import { ServiceCatalogService } from './service-catalog.service';
 
 describe('ServiceCatalogController', () => {
   let controller: ServiceCatalogController;
 
+  // Mock service
+  const mockServiceCatalogService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ServiceCatalogController],
+      providers: [
+        {
+          provide: ServiceCatalogService,
+          useValue: mockServiceCatalogService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ServiceCatalogController>(ServiceCatalogController);
