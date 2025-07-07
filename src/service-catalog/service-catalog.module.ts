2b0ff176150018d85be8a2cdf22695c7d7368242
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-07-07 11:06:12 +0300

    feat: SERVICES

--- src/service-catalog/service-catalog.module.ts
+++ src/service-catalog/service-catalog.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ServiceCatalogController } from './service-catalog.controller';
 import { ServiceCatalogService } from './service-catalog.service';
+import { ServiceCatalogService } from './service-catalog.service';
 
 @Module({
   controllers: [ServiceCatalogController],
