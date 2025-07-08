482d7e7198c33def697261bfbc0a89837dff11f1
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2025-07-08 23:15:55 +0300

    feat: implement FilesService with Cloudinary configuration

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { v2 as cloudinary } from 'cloudinary';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  constructor(private cfg: ConfigService) {
+    cloudinary.config({
+      cloud_name: cfg.get('dnsnkx7ex'),
+      api_key: cfg.get('534815873559974'),
+      api_secret: cfg.get('V59vER0y5pEW5EKBpi_v7Fqgj1k'),
+    });
+  }
+  // …
+}
